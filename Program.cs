f5c155bea186d64cdbbb0455c87cae5985cdd35e
Laurence Elsdon <github@elsdon.me>
2020-08-08 15:00:02 +0100

    BlockingCollection Implementation

--- Program.cs
+++ Program.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pipeline
+namespace Flow
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BlockingCollectionExample();
+
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+        }
+
+        static void BlockingCollectionExample()
+        {
+            var builder = new CastingPipelineBuilder();
+
+            //casting from object is needed on each step
+            builder.AddStep(input => FindMostCommon(input as string));
+            builder.AddStep(input => (input as string).Length);
+            builder.AddStep(input => ((int)input) % 2 == 1);
+
+            var pipeline = builder.GetPipeline();
+
+            pipeline.Finished += res => Console.WriteLine(res);
+            pipeline.Execute("The pipeline pattern is the best pattern");
+            // 'True' is printed because 'pattern' is the most common with 7 chars and it's an odd number
+            // ...
+        }
+
+
+        static string FindMostCommon(string input)
+        {
+            return input.Split(' ')
+                .GroupBy(word => word)
+                .OrderBy(group => group.Count())
+                .Last()
+                .Key;
         }
     }
 }
