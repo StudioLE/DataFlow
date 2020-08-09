b5958bb3ddbeb3cb94048086bf39639ab7ca1eee
Laurence Elsdon <github@elsdon.me>
2020-08-09 10:42:25 +0100

    Await the result

--- Flow.cs
+++ Flow.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace Flow
 {
-    class Flow {
+    public class TC<TInput, TOutput>
+    {
+        public TC(TInput input, TaskCompletionSource<TOutput> tcs)
+        {
+            Input = input;
+            TaskCompletionSource = tcs;
+        }
+        public TInput Input { get; set; }
+        public TaskCompletionSource<TOutput> TaskCompletionSource { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,15 +11,15 @@ namespace Flow
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            BlockingCollectionExample();
+            await BlockingCollectionExampleAsync();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
 
-        static void BlockingCollectionExample()
+        static async Task BlockingCollectionExampleAsync()
         {
             var solids = new List<Solid>()
             {
@@ -38,37 +38,81 @@ namespace Flow
                 new Cylinder(3, 6)   { Color = Color.Lavender } // 12
             };
 
-            var flow = CreateFlow(
-                resultCallback: res => Console.WriteLine(res));
-            
-            flow.Post(solids);
+            var flow = CreateFlow();
+
+            var tcs = new TaskCompletionSource<KnownColor>();
+
+            var tc = new TC<List<Solid>, KnownColor>(solids, tcs);
+
+            var task = tcs.Task;
+
+            await flow.SendAsync(tc);
+
+            var result = await task;
+
+            Console.WriteLine(result);
             // After execution completes we should see "Fuchsia" printed to the console
         }
 
-        public static TransformBlock<List<Solid>, List<Cuboid>> CreateFlow(Action<KnownColor> resultCallback)
+        public static TransformBlock<TC<List<Solid>, KnownColor>, TC<List<Cuboid>, KnownColor>> CreateFlow()
         {
             // Cuboids only
-            var step1 = new TransformBlock<List<Solid>, List<Cuboid>>(input => CuboidFilter(input));
+            var step1 = new TransformBlock<TC<List<Solid>, KnownColor>, TC<List<Cuboid>, KnownColor>>(
+                tc => new TC<List<Cuboid>, KnownColor> (
+                    CuboidFilter(tc.Input),
+                    tc.TaskCompletionSource
+                )
+           );
 
             // Mass greater than 2
-            var step2 = new TransformBlock<List<Cuboid>, List<Cuboid>>(input => input.Where(x => x.Mass > 2).ToList());
+            var step2 = new TransformBlock<TC<List<Cuboid>, KnownColor>, TC<List<Cuboid>, KnownColor>>(
+                tc => new TC<List<Cuboid>, KnownColor>(
+                    tc.Input.Where(x => x.Mass > 2).ToList(),
+                    tc.TaskCompletionSource
+                )
+            );
 
             // Colours
-            var step3 = new TransformBlock<List<Cuboid>, List<Color>>(input => input.Select(x => x.Color).ToList());
+            var step3 = new TransformBlock<TC<List<Cuboid>, KnownColor>, TC<List<Color>, KnownColor>>(
+                tc => new TC<List<Color>, KnownColor>(
+                    tc.Input.Select(x => x.Color).ToList(),
+                    tc.TaskCompletionSource
+                )
+            );
 
             // At least half red
-            var step4 = new TransformBlock<List<Color>, List<Color>>(input => input.Where(x => x.R >= 128).ToList());
+            var step4 = new TransformBlock<TC<List<Color>, KnownColor>, TC<List<Color>, KnownColor>>(
+                tc => new TC<List<Color>, KnownColor>(
+                    tc.Input.Where(x => x.R >= 128).ToList(),
+                    tc.TaskCompletionSource
+                )
+            );
 
             // Order by blue
-            var step5 = new TransformBlock<List<Color>, List<Color>>(input => input.OrderByDescending(x => x.B).ToList());
+            var step5 = new TransformBlock<TC<List<Color>, KnownColor>, TC<List<Color>, KnownColor>>(
+                tc => new TC<List<Color>, KnownColor>(
+                    tc.Input.OrderByDescending(x => x.B).ToList(),
+                    tc.TaskCompletionSource
+                )
+            );
 
             // Highest blue
-            var step6 = new TransformBlock<List<Color>, Color>(input => input.First());
+            var step6 = new TransformBlock<TC<List<Color>, KnownColor>, TC<Color, KnownColor>>(
+                tc => new TC<Color, KnownColor>(
+                    tc.Input.First(),
+                    tc.TaskCompletionSource
+                )
+            );
 
             // Name of the colour
-            var step7 = new TransformBlock<Color, KnownColor>(input => input.ToKnownColor());
+            var step7 = new TransformBlock<TC<Color, KnownColor>, TC<KnownColor, KnownColor>>(
+                tc => new TC<KnownColor, KnownColor>(
+                    tc.Input.ToKnownColor(),
+                    tc.TaskCompletionSource
+                )
+            );
 
-            var callbackStep = new ActionBlock<KnownColor>(resultCallback);
+            var setResultStep = new ActionBlock<TC<KnownColor, KnownColor>>(tc => tc.TaskCompletionSource.SetResult(tc.Input));
 
             step1.LinkTo(step2, new DataflowLinkOptions());
             step2.LinkTo(step3, new DataflowLinkOptions());
@@ -76,7 +120,7 @@ namespace Flow
             step4.LinkTo(step5, new DataflowLinkOptions());
             step5.LinkTo(step6, new DataflowLinkOptions());
             step6.LinkTo(step7, new DataflowLinkOptions());
-            step7.LinkTo(callbackStep);
+            step7.LinkTo(setResultStep, new DataflowLinkOptions());
             return step1;
         }
 
