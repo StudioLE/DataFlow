1aef6c040e922bb8e6c4092492e8d4b359f184ea
Laurence Elsdon <github@elsdon.me>
2020-08-26 20:21:38 +0100

    Renamed Methods

--- DataFlow/DataFlow.cs
+++ DataFlow/DataFlow.cs
@@ -21,7 +21,7 @@ namespace StudioLE.DataFlow
     public class DataFlow<TIn, TOut>
     {
         private List<IDataflowBlock> _transformBlocks = new List<IDataflowBlock>();
-        public DataFlow<TIn, TOut> AddStep<TLocalIn, TLocalOut>(Func<TLocalIn, TLocalOut> stepFunc)
+        public DataFlow<TIn, TOut> Add<TLocalIn, TLocalOut>(Func<TLocalIn, TLocalOut> stepFunc)
         {
             var step = new TransformBlock<TC<TLocalIn, TOut>, TC<TLocalOut, TOut>>((tc) =>
             {
@@ -51,7 +51,7 @@ namespace StudioLE.DataFlow
             return this;
         }
 
-        public DataFlow<TIn, TOut> CreateFlow()
+        public DataFlow<TIn, TOut> Create()
         {
             var setResultStep =
                 new ActionBlock<TC<TOut, TOut>>((tc) => tc.TaskCompletionSource.SetResult(tc.Input));
