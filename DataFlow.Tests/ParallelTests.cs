e1189e0998cc868ce8d9c5f30ab19796f1a8e943
Laurence Elsdon <github@elsdon.me>
2020-08-29 13:37:46 +0100

    Added BoundedCapacity Test

--- DataFlow.Tests/ParallelTests.cs
+++ DataFlow.Tests/ParallelTests.cs
@@ -67,5 +67,52 @@ namespace DataFlow.Tests
             Console.WriteLine();
             Assert.IsTrue(Utils.CompareLists(expect, endTimes), "Expected DataFlows to complete execution in specific time");
         }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        public void BoundedCapacity(int boundedCapacity)
+        {
+            // Create a DataFlow
+            var dataFlow = new DataFlow<Monitor, Monitor>()
+                {
+                    MaxDegreeOfParallelism = -1,
+                    BoundedCapacity = boundedCapacity
+                }
+                .Add<Monitor, Monitor>(x => x.SetStart())
+                .Add<Monitor, Monitor>(Utils.Wait)
+                .Add<Monitor, Monitor>(Utils.Wait)
+                .Add<Monitor, Monitor>(x => x.SetEnd());
+
+            // Execute the DataFlow for all monitors
+            List<Task<Monitor>> tasks = Monitors.Select(dataFlow.Execute).ToList();
+
+            // Wait for all tasks to complete
+            Task.WaitAll(tasks.ToArray());
+
+            // Extract the results
+            List<Monitor> results = tasks.Select(x => x.Result).ToList();
+
+            // Extract the end times
+            List<double> endTimes = results.Select(x => Utils.RoundToPointFive(x.End)).ToList();
+
+            // Print all with Monitor.ToString()
+            results.ForEach(Console.WriteLine);
+
+            // Expected results
+            List<double> expect = Utils.SteppedRange(1, (Count / boundedCapacity), 0.5)
+                .SelectMany(x => Enumerable.Repeat(x, boundedCapacity))
+                .ToList();
+            //List<double> expect = new List<double>
+            //{
+            //    1,   1,   1,
+            //    1.5, 1.5, 1.5,
+            //    2,   2,   2,
+            //    2.5, 2.5, 2.5
+            //};
+            Console.WriteLine();
+            Assert.IsTrue(Utils.CompareLists(expect, endTimes), "Expected DataFlows to complete execution in specific time");
+        }
     }
 }
